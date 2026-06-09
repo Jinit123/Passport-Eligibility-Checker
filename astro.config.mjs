45793d87b6783a213ccf0bb702b7131979ee4900
Jinit Mewada <jinitmewada41@gmail.com>
2026-06-09 19:42:18 +0530

    Domain Name Changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from '@astrojs/react';
 
 export default defineConfig({
 
-  site: "https://example.com",
+  site: " https://passporteligibilitychecker.vercel.app",
 
   vite: {
     plugins: [tailwindcss()]
